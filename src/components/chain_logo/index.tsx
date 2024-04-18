482e010e1b20a34532d340b60b4a655e630777f7
nullgravity <github@nullgravity.io>
2024-04-18 14:23:23 +0200

    feat: icons

--- src/components/chain_logo/index.tsx
+++ src/components/chain_logo/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import light from '@assets/chain-logo-light.svg?url';
-import dark from '@assets/chain-logo-dark.svg?url';
-import classnames from 'classnames';
-import { useStyles } from '../chain_icon/useStyles';
+import React from "react";
+import light from "@assets/empeiria_light.svg?url";
+import dark from "@assets/empeiria_dark.svg?url";
+import classnames from "classnames";
+import { useStyles } from "../chain_icon/useStyles";
 
-const Logo = ({
-  className, src, ...props
-}: JSX.IntrinsicElements['img']) => {
+const Logo = ({ className, src, ...props }: JSX.IntrinsicElements["img"]) => {
   const classes = useStyles();
   return (
     <>
-      <img src={light} alt="" {...props} className={classnames(className, classes.light)} />
-      <img src={dark} alt="" {...props} className={classnames(className, classes.dark)} />
+      <img
+        src={light}
+        alt=""
+        {...props}
+        className={classnames(className, classes.light)}
+      />
+      <img
+        src={dark}
+        alt=""
+        {...props}
+        className={classnames(className, classes.dark)}
+      />
     </>
   );
 };
